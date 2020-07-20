71160d8dcd286afe6cee5e9b0e25bc13de90f916
GopikrishnaVadlamudi <gopi.vadlamudi10@gmail.com>
2020-07-20 22:40:10 +0530

    4 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -30,27 +30,27 @@ export class AppComponent {
       this.loading = false;
     });
 
-    this.representatives = [
-      { name: "Amy Elsner", image: "amyelsner.png" },
-      { name: "Anna Fali", image: "annafali.png" },
-      { name: "Asiya Javayant", image: "asiyajavayant.png" },
-      { name: "Bernardo Dominic", image: "bernardodominic.png" },
-      { name: "Elwin Sharvill", image: "elwinsharvill.png" },
-      { name: "Ioni Bowcher", image: "ionibowcher.png" },
-      { name: "Ivan Magalhaes", image: "ivanmagalhaes.png" },
-      { name: "Onyama Limba", image: "onyamalimba.png" },
-      { name: "Stephen Shaw", image: "stephenshaw.png" },
-      { name: "XuXue Feng", image: "xuxuefeng.png" }
-    ];
+    // this.representatives = [
+    //   { name: "Amy Elsner", image: "amyelsner.png" },
+    //   { name: "Anna Fali", image: "annafali.png" },
+    //   { name: "Asiya Javayant", image: "asiyajavayant.png" },
+    //   { name: "Bernardo Dominic", image: "bernardodominic.png" },
+    //   { name: "Elwin Sharvill", image: "elwinsharvill.png" },
+    //   { name: "Ioni Bowcher", image: "ionibowcher.png" },
+    //   { name: "Ivan Magalhaes", image: "ivanmagalhaes.png" },
+    //   { name: "Onyama Limba", image: "onyamalimba.png" },
+    //   { name: "Stephen Shaw", image: "stephenshaw.png" },
+    //   { name: "XuXue Feng", image: "xuxuefeng.png" }
+    // ];
 
-    this.statuses = [
-      { label: "Unqualified", value: "unqualified" },
-      { label: "Qualified", value: "qualified" },
-      { label: "New", value: "new" },
-      { label: "Negotiation", value: "negotiation" },
-      { label: "Renewal", value: "renewal" },
-      { label: "Proposal", value: "proposal" }
-    ];
+    // this.statuses = [
+    //   { label: "Unqualified", value: "unqualified" },
+    //   { label: "Qualified", value: "qualified" },
+    //   { label: "New", value: "new" },
+    //   { label: "Negotiation", value: "negotiation" },
+    //   { label: "Renewal", value: "renewal" },
+    //   { label: "Proposal", value: "proposal" }
+    // ];
   }
 
   onActivityChange(event) {
@@ -64,24 +64,24 @@ export class AppComponent {
     }
   }
 
-  onDateSelect(value) {
-    this.table.filter(this.formatDate(value), "date", "equals");
-  }
+  // onDateSelect(value) {
+  //   this.table.filter(this.formatDate(value), "date", "equals");
+  // }
 
-  formatDate(date) {
-    let month = date.getMonth() + 1;
-    let day = date.getDate();
+  // formatDate(date) {
+  //   let month = date.getMonth() + 1;
+  //   let day = date.getDate();
 
-    if (month < 10) {
-      month = "0" + month;
-    }
+  //   if (month < 10) {
+  //     month = "0" + month;
+  //   }
 
-    if (day < 10) {
-      day = "0" + day;
-    }
+  //   if (day < 10) {
+  //     day = "0" + day;
+  //   }
 
-    return date.getFullYear() + "-" + month + "-" + day;
-  }
+  //   return date.getFullYear() + "-" + month + "-" + day;
+  // }
 
   onRepresentativeChange(event) {
     this.table.filter(event.value, "representative", "in");
